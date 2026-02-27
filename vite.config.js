6df3dc82d504a30bc993cbb19e37018e02576f00
robdeer1 <r_deer@fanshaweonline.ca>
2026-02-26 20:05:33 -0500

    updated vite.config.js file to include all html pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        aboutMe: resolve(__dirname, "about-me.html"),
+        aboutMusic: resolve(__dirname, "about-music-therapy.html"),
+        contact: resolve(__dirname, "contact.html"),
+        programs: resolve(__dirname, "programs-services.html"),
+        privacy: resolve(__dirname, "privacy-policy.html"),
+      },
+    },
+  },
 });
